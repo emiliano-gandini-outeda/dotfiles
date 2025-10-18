f11333ef8790fed8a0e63cc4476a07e8b674061b
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-10-18 19:12:34 -0300

    More configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     neofetch
+    set -x FZF_DEFAULT_COMMAND "find /home/eclipse -type f -or -type d -name '.*'"
 end
---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/eclipse/.config/omarchy/current/theme/neovim.lua
